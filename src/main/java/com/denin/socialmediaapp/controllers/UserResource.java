bc260b3177f737714f621fb2ccfda370e0d2735a
denin <navdeep.beniwal1997@gmail.com>
2021-09-28 15:43:43 +0530

    Step 11 Complete

--- src/main/java/com/denin/socialmediaapp/controllers/UserResource.java
+++ src/main/java/com/denin/socialmediaapp/controllers/UserResource.java
@@ -2,9 +2,13 @@ package com.denin.socialmediaapp.controllers;
 
 import com.denin.socialmediaapp.models.User;
 import com.denin.socialmediaapp.models.UserDaoService;
+import com.denin.socialmediaapp.models.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -20,14 +24,19 @@ public class UserResource {
 
     @GetMapping(path = "/users/{id}")
     public User retrieveUser(@PathVariable Integer id){
-        return userDaoService.findOne(id);
+        User user = userDaoService.findOne(id);
+        if(user == null)
+            throw new UserNotFoundException("id-" + id);
+
+        return user;
     }
 
     @PostMapping(path = "/users")
-    public void createUser(@RequestBody User user){
+    public ResponseEntity<User> createUser(@RequestBody User user){
         User savedUser = userDaoService.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
     }
-
-
-
 }
